f003d7d17171547f3122ef1368faf556cd74abc4
matsmolinski <matsmolinski@gmail.com>
2020-05-13 22:22:28 +0200

    Working relationships

--- webapp/main.py
+++ webapp/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint, request, Response, render_template, url_for, redirect, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
+from datetime import date
 import requests
 import json
 
@@ -15,7 +16,7 @@ class Player(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     nickname = db.Column(db.String(50))
-    nationality = db.Column(db.Integer, nullable=False)
+    nationality = db.Column(db.String(30), nullable=False)
     turned_pro = db.Column(db.DateTime, nullable=False)
     highest_break = db.Column(db.Float, nullable=False)
 
@@ -31,24 +32,78 @@ class Player(db.Model):
         self.highest_break = hibreak
         self.nickname = nickname
 
-class Frame(db.Model):
+
+class Tournament(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30))
+    date_from = db.Column(db.DateTime)
+    date_to = db.Column(db.DateTime)
+    venue = db.Column(db.String(30))
+    ranked = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return '<' + self.name + '>'
+
+    def __init__(self, name, date_from, date_to, venue, ranked):
+        self.id = None
+        self.name = name
+        self.date_from = date_from
+        self.date_to = date_to
+        self.venue = venue
+        self.ranked = ranked
+
+
+class Match(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     player_one_id = db.Column(db.Integer, db.ForeignKey("player.id"))
     player_two_id = db.Column(db.Integer, db.ForeignKey("player.id"))
-    player_one = db.relationship("Player", foreign_keys=[player_one_id])
-    player_two = db.relationship("Player", foreign_keys=[player_two_id])
+    tournament_id = db.Column(db.Integer, db.ForeignKey("tournament.id"))
+    player_one = db.relationship("Player", foreign_keys=[
+                                 player_one_id], backref="matches_p1")
+    player_two = db.relationship("Player", foreign_keys=[
+                                 player_two_id], backref="matches_p2")
+    tournament = db.relationship("Tournament", backref="matches")
     score = db.Column(db.String(7))
     winner = db.Column(db.Integer)
-    
-    def __repr__(self):
-        return '< ' + self.player_one.first_name + ' ' + self.player_one.last_name + ' ' + self.score + ' ' + self.player_two.first_name + ' ' + self.player_two.last_name +'>'
+    t_round = db.Column(db.String(5))
 
-    def __init__(self, player_one_id, player_two_id, score, ):
+    def __repr__(self):
+        return '< ' + self.player_one.first_name + ' ' + self.player_one.last_name + ' ' + self.score + ' ' + self.player_two.first_name + ' ' + self.player_two.last_name + '>'
+
+    def __init__(self, tournament_id, player_one_id, player_two_id, score, winner, t_round):
         self.id = None
+        self.tournament_id = tournament_id
         self.player_one_id = player_one_id
         self.player_two_id = player_two_id
         self.score = score
         self.winner = winner
+        self.t_round = t_round
+
+
+class Frame(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    player_one_id = db.Column(db.Integer, db.ForeignKey("player.id"))
+    player_two_id = db.Column(db.Integer, db.ForeignKey("player.id"))
+    match_id = db.Column(db.Integer, db.ForeignKey("match.id"))
+    player_one = db.relationship("Player", foreign_keys=[
+                                 player_one_id], backref="frames_p1")
+    player_two = db.relationship("Player", foreign_keys=[
+                                 player_two_id], backref="frames_p2")
+    match = db.relationship("Match", backref="frames")
+    score = db.Column(db.String(7))
+    winner = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '< ' + self.player_one.first_name + ' ' + self.player_one.last_name + ' ' + self.score + ' ' + self.player_two.first_name + ' ' + self.player_two.last_name + '>'
+
+    def __init__(self, match_id, player_one_id, player_two_id, score, winner):
+        self.id = None
+        self.match_id = match_id
+        self.player_one_id = player_one_id
+        self.player_two_id = player_two_id
+        self.score = score
+        self.winner = winner
+
 
 class Break(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -59,7 +114,7 @@ class Break(db.Model):
     frame = db.relationship("Frame", backref="breaks")
 
     def __repr__(self):
-        return '< ' + self.score + ' by '+ self.author.first_name + ' ' + self.author.last_name + '>'
+        return '< ' + self.score + ' by ' + self.author.first_name + ' ' + self.author.last_name + '>'
 
     def __init__(self, score, author_id, frame_id):
         self.id = None
@@ -68,7 +123,25 @@ class Break(db.Model):
         self.score = score
 
 
+db.drop_all(bind=None)
 db.create_all()
+
+db.session.add(Tournament('Welsh open', date(2020, 2, 10),
+                          date(2020, 2, 16), 'Motorpoint Arena', True))
+db.session.add(Player('Neil', 'Robertson', 'Australia', date(
+    1998, 6, 1), 147, nickname="The Thunder from Down Under"))
+db.session.add(Player('Jamie', 'Clarke', 'Welsh', date(2018, 6, 1), 127))
+db.session.add(Match(1, 1, 2, '4-2', 1, 'L128'))
+
+db.session.add(Frame(1, 1, 2, '20-57', 2))
+db.session.add(Frame(1, 1, 2, '68-34', 1))
+db.session.add(Frame(1, 1, 2, '31-59', 2))
+db.session.add(Frame(1, 1, 2, '89-21', 1))
+db.session.add(Frame(1, 1, 2, '63-62', 1))
+db.session.add(Frame(1, 1, 2, '79-1', 1))
+db.session.add(Break(71, 1, 4))
+db.session.add(Break(51, 1, 6))
+db.session.commit()
 
 
 @app.route('/', methods=['GET'])
@@ -107,6 +180,14 @@ def upload_survey():
 @app.route('/thanks', methods=['GET'])
 def get_thanks():
     return render_template("thanks.html")
+
+
+@app.route('/test', methods=['GET'])
+def get_test():
+    sth = Match.query.options(db.joinedload('frames'))
+    for match in sth:
+        print(match, [x for x in match.frames if x.winner == 1])
+    return 'ok', 200
 
 
 @app.errorhandler(404)
